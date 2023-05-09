9e9f4767b53260e8b00affd6aaee2ad5a3e08d65
tychovrahe <brunam@seznam.cz>
2023-05-09 10:39:34 +0200

    fixup! feat(core): support STM32F429 discovery board

--- core/site_scons/boards/discovery.py
+++ core/site_scons/boards/discovery.py
@@ -8,22 +8,22 @@ def configure(env, features_wanted, defines, sources):
     hw_model = get_hw_model_as_number('TDT1')
     hw_revision = 0
 
-    defines += [f'TREZOR_BOARD=\\"boards/{board}\\"', ]
-    defines += [f'HW_MODEL={hw_model}', ]
-    defines += [f'HW_REVISION={hw_revision}', ]
-    sources += [f'embed/trezorhal/displays/{display}', ]
+    defines += [f'TREZOR_BOARD=\\"boards/{board}\\"']
+    defines += [f'HW_MODEL={hw_model}']
+    defines += [f'HW_REVISION={hw_revision}']
+    sources += [f'embed/trezorhal/displays/{display}']
     sources += ['embed/trezorhal/displays/ili9341_spi.c']
     sources += ['embed/trezorhal/sdram.c']
 
     if "input" in features_wanted:
-        sources += ['embed/trezorhal/i2c.c', ]
-        sources += ['embed/trezorhal/touch/touch.c', ]
-        sources += ['embed/trezorhal/touch/stmpe811.c', ]
+        sources += ['embed/trezorhal/i2c.c']
+        sources += ['embed/trezorhal/touch/touch.c']
+        sources += ['embed/trezorhal/touch/stmpe811.c']
         features_available.append("touch")
 
     if "dma2d" in features_wanted:
-        defines += ["USE_DMA2D", ]
-        sources += ['embed/lib/dma2d_emul.c', ]
+        defines += ["USE_DMA2D"]
+        sources += ['embed/lib/dma2d_emul.c']
         features_available.append("dma2d")
 
     env.get('ENV')['TREZOR_BOARD'] = board
