5893e8222857a1f381d4a70d0184f92ad31bfd34
tychovrahe <brunam@seznam.cz>
2023-06-23 16:50:13 +0200

    refactor(core): restructure trezorhal, prepare to support more platforms
    [no changelog]

--- core/embed/firmware/main.c
+++ core/embed/firmware/main.c
@@ -58,7 +58,7 @@
 #include "i2c.h"
 #endif
 #ifdef USE_TOUCH
-#include "touch/touch.h"
+#include "touch.h"
 #endif
 #ifdef USE_SD_CARD
 #include "sdcard.h"
@@ -68,8 +68,8 @@
 #ifdef SYSTEM_VIEW
 #include "systemview.h"
 #endif
+#include "platform.h"
 #include "rng.h"
-#include "stm32.h"
 #include "supervise.h"
 #ifdef USE_SECP256K1_ZKP
 #include "zkp_context.h"
